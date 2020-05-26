b3d06cd18663f3de43fb730860ffe9ef60c351c9
Tuyetrinh VO <tuyetrinhvo@gmail.com>
2020-05-26 21:45:27 +0200

    Updates

---
+++ precache-manifest.e4f6388512ea139bbf47ab7b4f2936d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4214d89d785bdefa91b1261cde33fac5",
+    "url": "/react-memory-game/index.html"
+  },
+  {
+    "revision": "feb44047ce38ad2057c0",
+    "url": "/react-memory-game/static/css/main.67d2e6ef.chunk.css"
+  },
+  {
+    "revision": "9f26e8063c6729443b0e",
+    "url": "/react-memory-game/static/js/2.a5966ce0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-memory-game/static/js/2.a5966ce0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feb44047ce38ad2057c0",
+    "url": "/react-memory-game/static/js/main.2ac35cc0.chunk.js"
+  },
+  {
+    "revision": "5d3a482dd6ff576965f8",
+    "url": "/react-memory-game/static/js/runtime-main.2ec9517b.js"
+  }
+]);
