398910f0e7110648e3e2404cadc7c5db08612348
Tuyetrinh VO <tuyetrinhvo@gmail.com>
2020-06-05 17:10:06 +0200

    tests with chai and enzyme

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
 
 import App from './App';
+import GuessCount from './GuessCount';
 
-it('renders without crashing', () => {
-	const div = document.createElement('div');
-	ReactDOM.render(<App />, div);
+describe('<App />', () => {
+	it('render not empty', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.isEmptyRender()).to.equal(false);
+	});
+	it('class `memory` exists', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.exists('.memory')).to.equal(true);
+	});
+	it('contains a zero-guess counter', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper).to.contain(<GuessCount guesses={0} />);
+	});
+	it('has 36 cards', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.find('Card')).to.have.length(36);
+	});
 });
